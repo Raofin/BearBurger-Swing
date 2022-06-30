ad7af398ab72e8cc1b2b6a3e08d9c02fca0db66a
Raofin <raofin@hotmail.com>
2022-07-01 02:41:57 +0600

    Initial commit

---
+++ src/main/java/org/apwj/App.java
@@ -0,0 +1,24 @@
+package org.apwj;
+
+import com.formdev.flatlaf.intellijthemes.*;
+import org.apwj.ui.MainForm;
+
+import javax.swing.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        FlatOneDarkIJTheme.setup(); // set theme
+
+        JFrame frame = new JFrame("BearBurger");
+        frame.setContentPane(new MainForm());
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+}
